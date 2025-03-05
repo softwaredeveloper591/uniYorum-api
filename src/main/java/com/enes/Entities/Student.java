7886ba3089aaad237a28cd5e3ff206aaf96df8f2
Enes Sezer <116477172+softwaredeveloper591@users.noreply.github.com>
2025-03-05 09:29:09 +0300

    Initial commit

---
+++ src/main/java/com/enes/Entities/Student.java
@@ -0,0 +1,41 @@
+package com.enes.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "student")
+@AllArgsConstructor
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Username cannot be empty.")
+	@Size(max = 45,message = "Username cannot exceed 45 characters.")
+	@Column(name = "username", length = 45, nullable = false)
+	private String username;
+	
+	@Email(message = "Invalid email address.")
+	@Size(max = 45,message = "Email cannot exceed 45 characters.")
+	@Column(length=45, nullable = false, unique = true)
+	private String email;
+	
+	@Size(max = 250, message = "Password cannot exceed 250 characters.")
+	@Column(length = 250, nullable = false, unique = true)
+	private String password;
+	
+	
+}
---
+++ src/main/java/com/enes/uniyorum_api/UniyorumApiApplication.java
@@ -0,0 +1,13 @@
+package com.enes.uniyorum_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniyorumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniyorumApiApplication.class, args);
+	}
+
+}
